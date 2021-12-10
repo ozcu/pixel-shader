4fc7722f0e629e6e8bc1df255660988afe657383
Ugurhan Buyukozcu <ugurhan.buyukozcu@gmail.com>
2021-12-10 22:25:47 +0300

    Initial commit

--- src/script.js
+++ src/script.js
@@ -1,9 +1,19 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js'
+import GUI from 'lil-gui'
 
 import boilerVertexShader from './shaders/vertex.glsl'
 import boilerFragmentShader from './shaders/fragment.glsl'
+
+//Postprocessing
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js'
+
+//Import PixelShader.js
+import { PixelShader } from './PixelShader.js'
 
 /**
  * Base
@@ -13,7 +23,7 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-scene.background = new THREE.Color(0x132020)
+scene.background = new THREE.Color(0xffffff) //0x132020
 
 /**
  * Sizes
@@ -56,14 +66,14 @@ controls.enableDamping = true
  * Sphere
  */
 
-const geometry = new THREE.BoxBufferGeometry(1,1,1)
+/* const geometry = new THREE.BoxBufferGeometry(1,1,1)
 
 let shaderMaterial = null
 
 shaderMaterial= new THREE.ShaderMaterial({
     vertexShader:boilerVertexShader,
     fragmentShader:boilerFragmentShader,
-    wireframe:true,
+    wireframe:false,
     uniforms:{
         uTime:{value:0}
     }
@@ -73,7 +83,84 @@ shaderMaterial= new THREE.ShaderMaterial({
 
 const Sphere = new THREE.Mesh(geometry,shaderMaterial)
 
-scene.add(Sphere)
+scene.add(Sphere) */
+
+
+
+/**
+ * Textures
+ */
+ const textureLoader = new THREE.TextureLoader()
+ const matcapTexture = textureLoader.load('/textures/matcaps/12.png')
+ const matcapTexture2 = textureLoader.load('/textures/matcaps/3.png')
+ const matcapTexture3 = textureLoader.load('/textures/matcaps/10.png')
+ const matcapTexture4 = textureLoader.load('/textures/matcaps/11.png')
+
+ const material = new THREE.MeshMatcapMaterial()
+ const material2 = new THREE.MeshMatcapMaterial()
+ const material3 = new THREE.MeshMatcapMaterial()
+ const material4 = new THREE.MeshMatcapMaterial()
+
+ material.matcap = matcapTexture
+ material2.matcap = matcapTexture2
+ material3.matcap = matcapTexture3
+ material4.matcap = matcapTexture4
+
+/**
+ * Object
+ */
+
+const loader = new OBJLoader()
+
+ loader.load(
+	// resource URL
+	'models/hand.obj',
+	// called when resource is loaded
+	function ( object ) {
+
+		scene.add( object );
+        object.children[0].material = material
+        object.position.x=-20
+        object.position.z = -5
+        object.scale.set(0.5,0.5,0.5)
+
+        const object2 = object.clone()
+        object2.position.x = -10
+        object.position.z = -5
+        object2.scale.set(0.5,0.5,0.5)
+        object2.children[0].material = material2
+        scene.add(object2)
+
+        const object3 = object.clone()
+        object3.position.x = 0
+        object.position.z = -5
+        object3.scale.set(0.5,0.5,0.5)
+        object3.children[0].material = material3
+        scene.add(object3)
+
+        const object4 = object.clone()
+        object4.position.x = 10
+        object.position.z = -5
+        object4.scale.set(0.5,0.5,0.5)
+        object4.children[0].material = material4
+        scene.add(object4)
+
+
+	},
+	// called when loading is in progresses
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
+
 
 /**
  * Renderer
@@ -84,6 +171,20 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+//Init Postprocessing
+
+const composer = new EffectComposer( renderer )
+composer.addPass( new RenderPass( scene, camera ) )
+
+let effect = new ShaderPass( PixelShader )
+
+composer.addPass( effect )
+
+//GUI
+const gui = new GUI()
+gui.add(effect.uniforms.pixelSize,'value').min(1.0).max(10.0).step(0.01).name('PixelSize')
+
 
 /**
  * Animate
@@ -101,10 +202,17 @@ const animateScene = () =>
     controls.update()
 
     //Update shader with time
-    shaderMaterial.uniforms.uTime.value = elapsedTime
+    //shaderMaterial.uniforms.uTime.value = elapsedTime
+
+    //Effect
+    //  effect.uniforms[ 'time' ].value = elapsedTime * 0.3
+    //  effect.uniforms[ 'progress' ].value = 0.5
+    //effect.uniforms[ 'pixelSize' ].value = 3
 
     // Render
-    renderer.render(scene, camera)
+    //renderer.render(scene, camera)
+
+    composer.render()
 
     // Call animateScene again on the next frame
     window.requestAnimationFrame(animateScene)
